2102233972e3add10c4d123b70fa0bcbc28d1495
zhanghaoyang <zhanghaoyang.tomoko@bytedance.com>
2019-01-21 15:53:07 +0800

    feat(webglnoob): init webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		node: true,
+	},
+	extends: 'eslint:recommended',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'no-dupe-keys': ['error'],
+		'use-isnan': ['warn'],
+		'dot-location': ['warn'],
+		'no-useless-escape': ['error'],
+		'no-useless-return': ['error'],
+		'no-unused-vars': ['error'],
+	},
 };
